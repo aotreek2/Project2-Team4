82e70582eb71f06658e9ddbc9111a5d6269ccf9f
Jacob-Waugh <126210762+Jacob-Waugh@users.noreply.github.com>
2024-10-29 01:29:27 -0400

    Updated Main menu with panels and animations

--- Project 2 - Team 4/Assets/Scripts/MenuManager.cs
+++ Project 2 - Team 4/Assets/Scripts/MenuManager.cs
@@ -5,36 +5,73 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    [SerializeField] private GameObject helpPanel, creditsPanel;
+    [SerializeField] private GameObject helpPanel, creditsPanel, mainPanel;
+
+    Animator camAnimator;
 
     private void Start()
     {
         helpPanel.SetActive(false);
         creditsPanel.SetActive(false);
+        mainPanel.SetActive(true);
+
+        camAnimator = gameObject.GetComponent<Animator>();
+
+        camAnimator.ResetTrigger("PlayHit");
+        camAnimator.ResetTrigger("HelpHit");
+        camAnimator.ResetTrigger("CreditsHit");
+        camAnimator.ResetTrigger("BackHit");
     }
 
-    public void OnPLayButtonClicked()
+    public void OnPlayButtonClicked()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        camAnimator.SetTrigger("PlayHit");
+        camAnimator.ResetTrigger("BackHit");
+
+        //mainPanel.SetActive(false);
     }
     public void OnHelpButtonClicked()
     {
-        helpPanel.SetActive(true);
+        //helpPanel.SetActive(true);
+        camAnimator.SetTrigger("HelpHit");
+        camAnimator.ResetTrigger("BackHit");
+
+        //mainPanel.SetActive(false);
     }
     public void OnCreditsButtonClicked()
     {
-        creditsPanel.SetActive(true);
+        //creditsPanel.SetActive(true);
+        camAnimator.SetTrigger("CreditsHit");
+        camAnimator.ResetTrigger("BackHit");
+
+        //mainPanel.SetActive(false);
     }
-    public void OnCloseButtonClicked()
+    public void OnBackButtonClicked()
     {
+        /*
         if(helpPanel.activeSelf == true || creditsPanel.activeSelf == true)
         {
             helpPanel.SetActive(false);
             creditsPanel.SetActive(false);
         }
+        */
+        camAnimator.SetTrigger("BackHit");
+
+        camAnimator.ResetTrigger("PlayHit");
+        camAnimator.ResetTrigger("HelpHit");
+        camAnimator.ResetTrigger("CreditsHit");
+        
+
+        //mainPanel.SetActive(true);
     }
     public void OnQuitButtonClicked()
     {
         Application.Quit();
+    }
+
+    public void MainMenuFlipper()
+    {
+        mainPanel.SetActive(!mainPanel.activeSelf);
     }
 }
